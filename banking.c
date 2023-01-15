500ac26bf88fd5026cd2f5da99059a409f834cfe
Adam Milo <amilo@ultralig.ht>
2023-01-15 15:55:59 -0500

    update

--- banking.c
+++ banking.c
@@ -10,11 +10,12 @@ struct user{
 };
 
 int main(){
-	struct user usr;
+	struct user usr, usr1;
 	FILE *fp;
 	char filename[50], phone[50], pword[50];
-	int opt;
+	int opt, choice;
 	char cont = 'y';
+	float amount;
 
 	printf("\nWhat do you want to do?");
 	printf("\n\n1. Register an account");
@@ -62,11 +63,64 @@ int main(){
 			if(!strcmp(pword, usr.password)){
 				while(cont == 'y'){
 					system("clear");
-					printf("\nPress 1 to check balance");
+					printf("\nPress 1 for balance inquuiry");
 					printf("\nPress 2 to deposit an amount");
-					printf("\nPress 3 to withdraw");
-					printf("\nPress 4 to transfer the balance");
-					printf("\nPress 5 to change the password");
+					printf("\nPress 3 for cash withdrawal");
+					printf("\nPress 4 for online transfer");
+					printf("\nPress 5 for password change");
+					printf("\n\nYour choice:\t");
+					scanf("%d", &choice);
+
+					switch(choice){
+						case 1:
+							printf("\nYour current balance is Rs.%.2f", usr.balance);
+							break;
+						case 2:
+							printf("\nEnter the amount:\t");
+							scanf("%f", &amount);
+							usr.balance += amount;
+							fp = fopen(filename, "w");
+							fwrite(&usr, sizeof(struct user),1,fp);
+							if(&fwrite != NULL) printf("\nSuccessfully deposited.");
+							fclose(fp);
+							break;
+						case 3:
+							printf("\nEnter the amount:\t");
+							scanf("%f", &amount);
+							usr.balance -= amount;
+							fp = fopen(filename, "w");
+							fwrite(&usr, sizeof(struct user),1,fp);
+							if(&fwrite != NULL) printf("\nYou have withdrawn Rs.%.2f. ", amount);
+							fclose(fp);
+							break;
+						case 4:
+							printf("\nPlease enter the phone number to transfer the balance: \t");
+							scanf("%s", phone);
+							printf("\nPlease enger the amount to transfer:\t");
+							scanf("%f", &amount);
+							if(amount > usr.balance) printf("\nInsufficient balance");
+							else {
+								strcpy(filename, phone);
+								fp = fopen(strcat(filename, ".dat"), "r");
+								fread(&usr1, sizeof(struct user), 1,fp);
+								fclose(fp);
+								fp = fopen(filename, "w");
+								usr1.balance += amount;
+								fwrite(&usr1, sizeof(struct user),1,fp);
+
+								fclose(fp);
+								if(&fwrite != NULL){
+									printf("You have successfully transfered Rs.%.2f to %s", amount, phone);
+									strcpy(filename, usr.phone);
+									fp = fopen(strcat(filename, ".dat"), "w");
+									usr.balance -= amount;
+									fwrite(&usr, sizeof(struct user), 1,fp);
+									fclose(fp);
+								}
+							}
+
+					}
+
 
 					printf("\nDo you want to continue the transaction [y/n]\t");
 					scanf("%s", &cont);
@@ -74,12 +128,8 @@ int main(){
 			} else {
 				printf("\nInvalid password");
 			}
-
 		}
-		
-
 	}
-
 	return 0;
 }
 
